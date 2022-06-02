1a585df29554a4def18d89584a5b58898b4ed801
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-06-02 13:10:04 +0530

    Setup: Store globally

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+// Libraries
+import { Provider } from 'react-redux'
+
+
+// Components
+import { store } from './app/api/store'
+
 export default function Home() {
   return (
     <>
