8d6c635d0e1ee9bee433b0e0ab4270f928a065b2
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-06-02 15:02:49 +0530

    Updated: App.tsx with Routed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "../styles/globals.css";
 import { store } from "./app/api/store";
 
+import { Routes, Route } from "react-router-dom";
+import Layout from "../components/Layout";
+import Public from "../components/Public";
+import Login from "./features/auth/Login";
+import Welcome from "./features/auth/Welcome";
+import RequireAuth from "./features/auth/RequireAuth";
+// import UsersList from "../pages/features/";
+
 // Libraries
 import { Provider } from "react-redux";
 
@@ -10,6 +18,20 @@ function MyApp({ Component, pageProps }) {
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* public routes */}
+          <Route index element={<Public />} />
+          <Route path="login" element={<Login />} />
+
+          {/* protected routes */}
+          <Route element={<RequireAuth />}>
+            <Route path="welcome" element={<Welcome />} />
+            {/* <Route path="userslist" element={<UsersList />} /> */}
+          </Route>
+        </Route>
+      </Routes>
     </>
   );
 }
